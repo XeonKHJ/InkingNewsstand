5817bca162642cc6adbc1b33749d2be31214524a
康宏嘉 <redalertkhj@live.cn>
2019-02-18 15:04:27 +0800

    完成编写保存文件功能。

--- InkingNewstand/Classes/NewsPaper.cs
+++ InkingNewstand/Classes/NewsPaper.cs
@@ -26,50 +26,67 @@ namespace InkingNewstand
         /// </summary>
         public string PaperTitle { get; } = "未命名报纸";
 
+
         /// <summary>
-        /// 从文件中加载
+        /// 保存到文件
         /// </summary>
-        public async Task<StorageFile> SaveToFile(NewsPaper newsPaper)
+        /// <param name="newsPaper"></param>
+        /// <returns>要保存的报纸</returns>
+        static public async Task SaveToFile(NewsPaper newsPaper)
         {
             var storageFolder = ApplicationData.Current.LocalFolder;
 
-            //1、读取报纸列表数据
+            //0、打开文件
             string paperListFileName = "PaperList.dat";
             var paperListFile = await storageFolder.CreateFileAsync(paperListFileName, CreationCollisionOption.OpenIfExists);
-
             SortedDictionary<int, NewsPaper> paperListinFile = new SortedDictionary<int, NewsPaper>();
 
-            var stream = await paperListFile.OpenReadAsync(); //获取文件随机存取流
-            var inputStream = stream.GetInputStreamAt(0); //获取从0开始的输入流
-            var dataReader = new DataReader(inputStream); //在该输入流中附着一个数据读取器
-            uint loadBytes = await dataReader.LoadAsync((uint)stream.Size); //加载数据数据到中间缓冲区
-            byte[] bytes = new byte[(uint)stream.Size]; //用来存储从文件中读出的数据
-            paperListinFile = (SortedDictionary<int, NewsPaper>)ByteArrayToObject(bytes); //将读出的数据转换成SortedDictionary<int, NewsPaper>
+            //1、读取报纸列表数据
+            var stream = await paperListFile.OpenAsync(FileAccessMode.ReadWrite); //获取文件随机存取流
+            using (var inputStream = stream.GetInputStreamAt(0))//获取从0开始的输入流
+            { 
+                var dataReader = new DataReader(inputStream); //在该输入流中附着一个数据读取器
+                uint loadBytes = await dataReader.LoadAsync((uint)stream.Size); //加载数据数据到中间缓冲区
+                byte[] bytes = new byte[(uint)stream.Size]; //用来存储从文件中读出的数据
+                paperListinFile = (SortedDictionary<int, NewsPaper>)ByteArrayToObject(bytes); //将读出的数据转换成SortedDictionary<int, NewsPaper>
+            }
 
-            //2、获取当前报纸编号，若没有则新建
-            
-
+            //2、获取当前报纸编号
             ////2.1、如果文件中没有保存任何东西，则新建一个paperListinFile
             if (paperListinFile == null)
             {
                 paperListinFile = new SortedDictionary<int, NewsPaper>();
             }
-            ////如果有了则直接查询
-            
-            
 
-            //打开/创建保存该报纸的文件
-            string fileName = "sample.dat"; //修改成报纸名字
-            var saveFile = await storageFolder.CreateFileAsync(fileName, CreationCollisionOption.FailIfExists);
+            var paperEnumer = (from v in paperListinFile where v.Value.PaperTitle == newsPaper.PaperTitle select v);
+            ////2.2、如果文件中没有保存此添加
+            if(paperEnumer.Count() == 0)
+            {
+                paperListinFile.Add(paperListinFile.Count, newsPaper);
+            }
+            ////2.3、如果有则修改
+            else if(paperEnumer.Count() == 1)
+            {
+                foreach(var paper in paperEnumer)
+                {
+                    paperListinFile[paper.Key] = newsPaper;
+                }
+            }
+            ////2.4若查到有多张报纸有相同名字，则显示错误
+            else
+            {
+                throw new Exception("文件错误");
+            }
 
-            //读取文件
-            var buffer = new Windows.Storage.Streams.Buffer((uint)System.Runtime.InteropServices.Marshal.SizeOf(this));
-
-            await FileIO.WriteBufferAsync(saveFile, buffer);
-
-            return saveFile;
+            //3、将paperListinFile重新保存到文件中
+            await FileIO.WriteBytesAsync(paperListFile, ObjectToByteArray(paperListinFile));
         }
 
+        /// <summary>
+        ///  ///byte数组转换成object
+        /// </summary>
+        /// <param name="arrBytes"></param>
+        /// <returns>字节数组</returns>
         private static Object ByteArrayToObject(byte[] arrBytes)
         {
             using (var memStream = new System.IO.MemoryStream())
@@ -79,6 +96,21 @@ namespace InkingNewstand
                 memStream.Seek(0, System.IO.SeekOrigin.Begin);
                 var obj = binForm.Deserialize(memStream);
                 return obj;
+            }
+        }
+
+        /// <summary>
+        /// object转换成byte数组
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns>Object</returns>
+        private static byte[] ObjectToByteArray(Object obj)
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            using (var ms = new System.IO.MemoryStream())
+            {
+                bf.Serialize(ms, obj);
+                return ms.ToArray();
             }
         }
 
