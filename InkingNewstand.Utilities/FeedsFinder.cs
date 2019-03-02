a443b42be1331c8478958e0ad7002a86b964bb7f
康宏嘉 <redalertkhj@live.cn>
2019-03-02 20:00:18 +0800

    添加从网站查找订阅源方法。

---
+++ InkingNewstand.Utilities/FeedsFinder.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InkingNewstand.Utilities
+{
+    class FeedsFinder
+    {
+        public static List<Uri> GetFeedFromUrl(Uri url)
+        {
+            List<Uri> feedUrls = new List<Uri>(); //存储找到的订阅源的地址。
+            //to-do
+            return feedUrls;
+        }
+    }
+}
