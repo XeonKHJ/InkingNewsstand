c58c64c6b47be6fc5e650a5869d9531966e2c3f3
康宏嘉 <redalertkhj@live.cn>
2019-03-03 22:14:10 +0800

    重写NewsItem，非静态对象全是可序列化。

--- InkingNewstand/Classes/NewsItem.cs
+++ InkingNewstand/Classes/NewsItem.cs
@@ -11,56 +11,77 @@ namespace InkingNewstand
 {
     public class NewsItem
     {
-        private SyndicationItem item;
+        private static SyndicationItem staticItem;
         public NewsItem(SyndicationItem item, Uri url, string paperTitle)
         {
-            this.item = item;
+            staticItem = item;
+
+            //设置新闻链接
             NewsLink = url;
+
+            //该新闻报纸的标题
             PaperTitle = paperTitle;
-            //NewsLinks = item.ItemUri ?? item.Links.Select(l => l.Uri).FirstOrDefault();
+
+            //设置保存在订阅源中的该新闻的HTML
+            if (staticItem.Content != null)
+            {
+                InnerHTML = staticItem.Content.Text;
+            }
+            else if (staticItem.NodeValue != null)
+            {
+                InnerHTML = staticItem.NodeValue;
+            }
+            else InnerHTML = null;
+
+            //设置新闻标题
+            Title = staticItem.Title.Text;
+
+            //设置新闻简要
+            Summary = (staticItem.Summary != null) ? (staticItem.Summary.Text) : null;
+
+            //设置出版日期
+            PublishedDate = staticItem.PublishedDate.ToString();
+
+            //设置Content
+            if (staticItem.Content != null)
+            {
+                Content = staticItem.Content.Text;
+            }
+            else
+            {
+                Content = staticItem.Summary.Text;
+            }
+
+            //设置新闻作者
+            string authorsString = "";
+            for (int i = 0; i < staticItem.Authors.Count; ++i)
+            {
+                authorsString += staticItem.Authors[i].Name;
+            }
+            Authors = authorsString;
         }
 
-        public override string ToString()
-        {
-            return item.Title.Text.ToString();
-        }
+        public string Content { get; set; }
+
+
 
         public SyndicationItem Item
         {
-            get { return item; }
+            get { return staticItem; }
         }
 
-        public string Title
-        {
-            get { return item.Title.Text; }
-        }
+        public string Title { get; private set; }
 
-        public string Summary
-        {
-            get { return (item.Summary != null)?(item.Summary.Text):null; }
-        }
+        public string Summary { get; private set; }
 
-        public string PublishedDate
-        {
-            get { return (item.PublishedDate.ToString()); }
-        }
+        public string PublishedDate { get; private set; }
 
         public Uri NewsLink { private set; get; }
 
         public string PaperTitle { set; get; }
 
-        public string Authors
-        {
-            get
-            {
-                string authorsString = "";
-                for(int i = 0; i < item.Authors.Count; ++i)
-                {
-                    authorsString += item.Authors[i].Name;
-                }
-                return authorsString;
-            }
-        }
+        public string Authors { get; private set; }
+
         public string CoverUrl
         {
             get
@@ -69,20 +90,47 @@ namespace InkingNewstand
             }
         }
 
-        public string InnerHTML
+        public string InnerHTML { get; private set; }
+
+        public override string ToString()
         {
-            get
+            return staticItem.Title.Text.ToString();
+        }
+
+        /// <summary>
+        /// 重载运算符==
+        /// </summary>
+        /// <param name="lhs">左操作数</param>
+        /// <param name="rhs">右操作数</param>
+        /// <returns>是否等价</returns>
+        public static bool operator ==(NewsItem lhs, NewsItem rhs)
+        {
+            try
             {
-                if (item.Content != null)
+                if (lhs.Title == rhs.Title && lhs.PublishedDate == rhs.PublishedDate)
                 {
-                    return item.Content.Text;
+                    return true;
                 }
-                else if (item.NodeValue != null)
+                else
                 {
-                    return item.NodeValue;
+                    return false;
                 }
-                else return null;
             }
+            catch(NullReferenceException)
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// 重载运算符!=
+        /// </summary>
+        /// <param name="lhs">左操作数</param>
+        /// <param name="rhs">右操作数</param>
+        /// <returns>是否不等价</returns>
+        public static bool operator !=(NewsItem lhs, NewsItem rhs)
+        {
+            return !(lhs == rhs);
         }
     }
 }
